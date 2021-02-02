6a70dc1ca5070e4cd8ae6e8470854e8c6f592e54
Zero <john.royer@gmail.com>
2021-02-02 12:09:02 +0800

    Implement own observer

--- app/Observers/ZeroplexObserver.php
+++ app/Observers/ZeroplexObserver.php
@@ -2,20 +2,23 @@
 
 namespace App\Observers;
 
-use Spatie\Crawler\CrawlObservers;
+use Spatie\Crawler\CrawlObservers\CrawlObserver;
 
 class ZeroplexObserver extends CrawlObserver
 {
     public function willCrawl(UriInterface $url)
     {
+        var_dump( strval($url) );
+        exit;
     }
 
-    public function crawler(
+    public function crawled(
         UriInterface $url,
         ResponseInterface $response,
         ?UriInterface $foundOnUrl = null
-    )
-    {
+    ) {
+        echo strval($url);
+        sleep(15);
     }
 
     public function crawlFailed(
@@ -23,6 +26,7 @@ class ZeroplexObserver extends CrawlObserver
         RequestException $RequestException,
         ?UriInterface $foundOnUrl = null
     ) {
+        echo 'Failed:  ' . $strval($url);
     }
 
     public function finishedCrawling()
