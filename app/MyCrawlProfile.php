4ca8cf76d7a6a614745bb4b1f5d4ac93e317f1a4
Zero <john.royer@gmail.com>
2021-02-02 13:15:20 +0800

    Save data only if the host is corrent

--- app/MyCrawlProfile.php
+++ app/MyCrawlProfile.php
@@ -2,12 +2,16 @@
 
 namespace App;
 
-use Spatie\Crawler\CrawlProfiles;
+use Spatie\Crawler\CrawlProfiles\CrawlProfile;
 use Psr\Http\Message\UriInterface;
 
 class MyCrawlProfile extends CrawlProfile
 {
     public function shouldCrawl(UriInterface $url): bool
     {
+        if ('blog.zeroplex.tw' == $url->getHost()) {
+            return true;
+        }
+        return false;
     }
 }
