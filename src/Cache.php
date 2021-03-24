9e431b0911a180bc26477e07ed3342a779bb6381
Zero <john.royer@gmail.com>
2021-03-24 17:55:57 +0800

    Fix cahce checker

--- src/Cache.php
+++ src/Cache.php
@@ -15,6 +15,6 @@ class Cache
     public static function has(string $url): bool
     {
         $hash = md5($url);
-        return isset(static::$list);
+        return key_exists($hash, static::$list);
     }
 }
