581c06118efe07dce94507160dd85dadf6dc40a2
Zero <john.royer@gmail.com>
2021-03-24 12:58:54 +0800

    Update profiler

--- src/BloggerProfile.php
+++ src/BloggerProfile.php
@@ -10,21 +10,17 @@ class BloggerProfile extends CrawlProfile
     public function shouldCrawl(UriInterface $url): bool
     {
         if ('blog.zeroplex.tw' != $url->getHost()) {
-            echo $url->getHost() . " ... passed\n";
             return false;
         }
 
         if (1 == preg_match('/\/search/', $url->getPath())) {
-            echo $url->getPath() . " ... passed\n";
             return false;
         }
 
         if (1 == preg_match('/\/feeds/', $url->getPath())) {
-            echo $url->getPath() . " ... passed\n";
             return false;
         }
 
-        echo $url->getHost() . $url->getPath() . " ... ok\n";
         return true;
     }
 }
