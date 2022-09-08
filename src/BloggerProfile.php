1b6083a379a8a147826c633b5dd5de669589ece2
Zero <john.royer@gmail.com>
2022-09-08 23:18:38 +0800

    update scan rules

--- src/BloggerProfile.php
+++ src/BloggerProfile.php
@@ -22,10 +22,6 @@ class BloggerProfile extends CrawlProfile
             return false;
         }
 
-        if (1 == preg_match('/\/page/', $url->getPath())) {
-            return false;
-        }
-
         // check if duplicated
         if (true == Cache::has(strval($url))) {
             return false;
@@ -33,14 +29,6 @@ class BloggerProfile extends CrawlProfile
 
         if (1 == preg_match('/^\/$/', $url->getPath())) {
             return true;
-        }
-
-        if (1 == preg_match('/\/tag\//', $url->getPath())) {
-            return true;
-        }
-
-        if (1 !== preg_match('/\/2021/', $url->getPath())) {
-            return false;
         }
 
         return true;
