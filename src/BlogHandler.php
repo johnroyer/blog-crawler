22d1492a53d35c8d93acd2ed75f78686108ae4b7
Zero <john.royer@gmail.com>
2023-01-12 13:05:45 +0800

    Add domain handler

---
+++ src/BlogHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Crawler;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class BlogHandler extends \Zeroplex\Crawler\Handler\AbstractHandler
+{
+    protected $count = 0;
+
+    /**
+     * @inheritDoc
+     */
+    public function getDomain(): string
+    {
+        return 'blog.zeroplex.tw';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function shouldFetch(RequestInterface $request): bool
+    {
+        if (1 === preg_match('/\/feed/', $request->getUri())) {
+            return false;
+        }
+        if (1 === preg_match('/\.(css|js|jpg|png|gif|webp|ico)/', $request->getUri())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function handle(ResponseInterface $response, RequestInterface $request): void
+    {
+        $this->count++;
+        echo sprintf(' %5s  ', $this->count)
+            . $request->getUri() . "\n";
+    }
+}
